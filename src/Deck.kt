10a321538060d3951c8297632004ab846ab0b551
Ben <bfdeitch@gmail.com>
2016-07-19 13:11:15 -0700

    Finished (long) Cards property.

--- src/Deck.kt
+++ src/Deck.kt
@@ -1,3 +1,13 @@
 class Deck {
-    val cards: Array<Card> = Array(52,)
+    val cards: Array<Card> = Array(52,
+            fun(i: Int): Card {
+                val value = i % 13
+                val suit = when(i / 13) {
+                    0 -> "Clubs"
+                    1 -> "Diamonds"
+                    2 -> "Hearts"
+                    else -> "Spades"
+                }
+                return Card(value, suit)
+            })
 }
