d2e65b05f9d4f596f043ec9c547421e9c62b3031
Ben <bfdeitch@gmail.com>
2016-07-19 13:26:30 -0700

    Finished Deck class.

--- src/Deck.kt
+++ src/Deck.kt
@@ -1,5 +1,8 @@
+import java.util.*
+
 class Deck {
     val cards = Array(52, { Card(it % 13, getSuit(it)) })
+    var cardsInDeck: MutableList<Card> = cards.toMutableList()
 
     private fun getSuit(i: Int) = when(i / 13) {
             0 -> "Clubs"
@@ -7,4 +10,11 @@ class Deck {
             2 -> "Hearts"
             else -> "Spades"
         }
+
+    fun drawCard(): Card = cardsInDeck.removeAt(0)
+
+    fun reset() {
+        cardsInDeck = cards.toMutableList()
+        Collections.shuffle(cardsInDeck)
+    }
 }
