36e0eddb7c7eadfccc967fe4b2e589f732d7bd88
Ben <bfdeitch@gmail.com>
2016-07-19 13:20:29 -0700

    Shortened 'cards' property.

--- src/Deck.kt
+++ src/Deck.kt
@@ -1,13 +1,10 @@
 class Deck {
-    val cards: Array<Card> = Array(52,
-            fun(i: Int): Card {
-                val value = i % 13
-                val suit = when(i / 13) {
-                    0 -> "Clubs"
-                    1 -> "Diamonds"
-                    2 -> "Hearts"
-                    else -> "Spades"
-                }
-                return Card(value, suit)
-            })
+    val cards = Array(52, { Card(it % 13, getSuit(it)) })
+
+    private fun getSuit(i: Int) = when(i / 13) {
+            0 -> "Clubs"
+            1 -> "Diamonds"
+            2 -> "Hearts"
+            else -> "Spades"
+        }
 }
