4ea1722bb962a167105a22e6597de263beb0560b
Ben <bfdeitch@gmail.com>
2016-07-19 14:13:50 -0700

    Finished Tableau.suitCheck(?,?) and Tableau.addCards().

--- src/TableauPile.kt
+++ src/TableauPile.kt
@@ -5,15 +5,25 @@ class TableauPile(var cards: MutableList<Card>) {
     }
 
     fun addCards(newCards: MutableList<Card>): Boolean {
-        if (newCards.first().value == cards.last().value - 1 &&
-                suitCheck(newCards.first(), cards.last())) {
-
+        if (cards.size > 0) {
+            if (newCards.first().value == cards.last().value - 1 &&
+                    suitCheck(newCards.first(), cards.last())) {
+                cards.addAll(newCards)
+                return true
+            }
+        } else if (newCards.first().value == 12) {
+            cards.addAll(newCards)
+            return true
         }
+        return false
     }
 
     private fun suitCheck(c1: Card, c2: Card): Boolean {
-
-
+        if ((redSuits.contains(c1.suit) && blackSuits.contains(c2.suit)) ||
+                (blackSuits.contains(c1.suit) && redSuits.contains(c2.suit))) {
+            return true
+        }
+        return false
     }
 
 }
